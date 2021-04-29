d91b7f84495d920e1d5cebeec369a6c70eefe3b5
yaroleks <30700060+yaroleks@users.noreply.github.com>
2021-04-29 11:16:16 +0300

    Update ToastMessageService.swift

--- ToastMessageService.swift
+++ ToastMessageService.swift
@@ -14,10 +14,22 @@ protocol ToastMessageService {
 	/// - Parameter topOffset: Used for setting a top offset on iPad.
 	/// - Parameter topOffset: Used for setting a bottom offset on iPhone.
 	func showToast(_ info: ToastMessageInfo, topOffset: CGFloat?, bottomOffset: CGFloat?)
+}
 
+extension ToastMessageService {
 	/// Calculates an offset for cases when there is a navigation controller
 	///
 	/// - Parameter navigationController: a current navigation controller
 	/// - Parameter window: a window that holds a current view
-	func topOffset(for navigationController: UINavigationController?, in window: UIWindow?) -> CGFloat?
+	func topOffset(for navigationController: UINavigationController?, in window: UIWindow?) -> CGFloat? {
+		func topOffset(for navigationController: UINavigationController?, in window: UIWindow?) -> CGFloat? {
+		guard let navigationController = navigationController,
+			  let window = window else {
+			return nil
+		}
+		let maxY = navigationController.navigationBar.frame.maxY
+
+		let origin = CGPoint(x: 0, y: maxY)
+		return navigationController.view.convert(origin, to: window).y
+	}
 }
